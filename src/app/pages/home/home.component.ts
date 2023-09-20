71f92b6b5089b2e737455804a7c37f2d55bd42ab
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-09-19 22:07:18 -0300

    feat: logout feature created on homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth/services/auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,4 +11,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
