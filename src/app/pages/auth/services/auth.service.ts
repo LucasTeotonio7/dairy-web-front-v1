a32cf32950dfd727e2640f9ba95a4431ea3964c4
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-09-19 22:00:08 -0300

    feat: created authentication service

---
+++ src/app/pages/auth/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+import { User } from '../models/user';
+import { TokenService } from './token.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  readonly apiUrl = "http://localhost:8100";
+
+  constructor(
+    private http: HttpClient, 
+    private tokenService: TokenService
+  ) { }
+
+  authenticate(user: User): Observable<any> {
+
+    return this.http.post<any>(`${this.apiUrl}/api-token-auth/`, user).pipe(
+      catchError((error) => {
+        console.error('Erro na autenticação:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  is_authenticated(): boolean  {
+    let token = this.tokenService.getToken();
+    return Boolean(token);
+  }
+  
+  logout(): void {
+    this.tokenService.removeToken();
+  }
+}
