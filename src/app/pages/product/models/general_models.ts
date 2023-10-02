582a78f99f996289ec6b3913a5421d0d14a87f6d
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2023-10-02 20:50:56 -0300

    feat: created general models

---
+++ src/app/pages/product/models/general_models.ts
@@ -0,0 +1,15 @@
+export interface Brand {
+    id: string;
+    description: string;
+}
+
+export interface Category {
+    id: string;
+    description: string;
+}
+
+export interface MeasureUnit {
+    id: string;
+    description: string;
+    abbreviation: string;
+}
