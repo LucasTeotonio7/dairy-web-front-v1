db0ea6b7823c37a7bae8cc6625daa359e4b676a3
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-04 21:34:18 -0300

    chore: updated locale to pt-br

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ProductFormComponent } from './pages/product/forms/product-form/product-form.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+registerLocaleData(ptBr)
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    ProductComponent
+    ProductComponent,
+    ProductFormComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +32,7 @@ import { ProductComponent } from './pages/product/product.component';
     HttpClientModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-PT' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
