f64aba9eaec1a83dc9ffd9b0f6d481ba534bb237
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-11 21:43:24 -0300

    feat: created product deletion

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from './models/product';
 import { ProductService } from './services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,33 @@ import { ProductService } from './services/product.service';
 })
 export class ProductComponent {
 
-  products: Product[] = []; 
+  products: Product[] = [];
+  productId!: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
-    this.productService.get().subscribe((products) => {
+    this.productService.list().subscribe((products) => {
       this.products = products;
+    });
+  }
+
+  setProductId($event: Event): void {
+    let button = $event.currentTarget as HTMLElement;
+    this.productId = button.id;
+  }
+
+  deleteProduct() {
+    this.productService.delete(this.productId).subscribe({
+      next: (response) => {
+        alert('produto excluído!');
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        this.ngOnInit();
+      }
     });
   }
 
