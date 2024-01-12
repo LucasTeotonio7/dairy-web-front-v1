aa7353affb154f9d47af8b632fef49d30ff84655
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-11 21:42:48 -0300

    feat: added product routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './pages/auth/guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login.component';
+import { ProductFormComponent } from './pages/product/forms/product-form/product-form.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent,
     canActivate: [authGuard],
     children: [
-      { path: 'products', component: ProductComponent }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: ProductComponent },
+      { path: 'products/create', component: ProductFormComponent },
+      { path: 'products/:id', component: ProductFormComponent }
     ],
   },
   { path: 'login', component: LoginComponent, canActivate: [authGuard] },
