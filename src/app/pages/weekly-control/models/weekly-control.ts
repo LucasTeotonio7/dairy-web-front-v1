aed63f721d40892cfb8034f0a164994ab856f9aa
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-28 21:04:36 -0300

    feat: created weekly record list

---
+++ src/app/pages/weekly-control/models/weekly-control.ts
@@ -0,0 +1,10 @@
+export interface WeeklyControl {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    start_date: string;
+    end_date: string;
+    is_closed: boolean;
+    product: string;
+    created_by: string;
+}
--- src/app/pages/weekly-control/weekly-control.component.ts
+++ src/app/pages/weekly-control/weekly-control.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Paginator } from 'src/app/shared/models/paginator';
+import { WeeklyControl } from './models/weekly-control';
+import { WeeklyControlService } from './services/weekly-control.service';
 
 @Component({
   selector: 'app-weekly-control',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./weekly-control.component.css']
 })
 export class WeeklyControlComponent {
+    weeklyControls: WeeklyControl[] = [];
+    weeklyControlId!: string;
+    paginator!: Paginator<WeeklyControl>;
 
+    constructor(private weeklyControlService: WeeklyControlService, private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.route.queryParams.subscribe(params => {
+            const page = params['page'] || 1;
+            this.loadWeeklyControls(page);
+        });
+    }
+
+    loadWeeklyControls(page: number) {
+        this.weeklyControlService.list(page).subscribe((paginator: Paginator<WeeklyControl>) => {
+            this.paginator = paginator;
+            this.weeklyControls = paginator.results;
+        });
+    }
+
+    setWeeklyControlId($event: Event): void {
+        let button = $event.currentTarget as HTMLElement;
+        this.weeklyControlId = button.id;
+    }
+
+    deleteWeeklyControl() {
+        this.weeklyControlService.delete(this.weeklyControlId).subscribe({
+          next: (response) => {
+            alert('Planilha excluída!');
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {
+            this.ngOnInit();
+          }
+        });
+    }
 }
