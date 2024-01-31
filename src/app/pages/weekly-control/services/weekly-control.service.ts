782dce9189cf749f64112e387f1c6d2b25fac57c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-01-30 21:08:18 -0300

    feat: created method to list products

--- src/app/pages/weekly-control/services/weekly-control.service.ts
+++ src/app/pages/weekly-control/services/weekly-control.service.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenService } from '../../auth/services/token.service';
 import { CrudService } from 'src/app/shared/services/crud.service';
 import { WeeklyControl } from '../models/weekly-control';
+import { Observable } from 'rxjs';
+import { Product } from '../../product/models/product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +14,14 @@ export class WeeklyControlService extends CrudService<WeeklyControl>  {
 
     constructor(http: HttpClient, tokenService: TokenService) {
         super(http, tokenService, 'weekly-control/');
+    }
+
+    getProducts(): Observable<Product[]> {
+        const headers = new HttpHeaders({
+          'Authorization': `Token ${this.tokenService.getToken()}`
+        });
+        const BASE_URL = environment.API;
+        const requestOptions = { headers: headers };
+        return this.http.get<Product[]>(`${BASE_URL}product/?paginate=false`, requestOptions);
     }
 }
