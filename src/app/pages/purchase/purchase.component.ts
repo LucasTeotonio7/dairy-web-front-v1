b2f1719e5d74df641d8c55da405d44dcf60c5749
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-02-27 20:05:21 -0300

    feat: rendered quantity values by supplier

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { WeeklyControlService } from './../weekly-control/services/weekly-control.service';
+import { WeeklyControl } from '../weekly-control/models/weekly-control';
+
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
@@ -8,10 +11,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PurchaseComponent {
   weeklyControlId : string | any = ''
+  weeklyControl!: WeeklyControl;
 
   constructor(
     private router: Router, 
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private weeklyControlService: WeeklyControlService
   ) {}
 
   ngOnInit() {
@@ -20,9 +25,44 @@ export class PurchaseComponent {
 
   getWeeklyControl() {
     this.route.paramMap.subscribe(params => {
-      this.weeklyControlId = params.get('id')
-      console.log(params.get('id'))
+      this.weeklyControlId = params.get('id');
+      let supplierId = params.get('supplier-id');
+      if (supplierId) {
+        let queryParam = { "supplier_id" : supplierId };
+        this.weeklyControlService.get(this.weeklyControlId, queryParam).subscribe({
+          next: (response) => {
+              this.weeklyControl = response;
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {}
+        });
+      }
     })
+  }
+
+
+  getInputValues() {
+
+    var purchases = [];
+    var inputs = document.getElementsByClassName("purchase-quantity");
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i] as HTMLInputElement;;
+      purchases.push({
+        reference_day: input.dataset['referenceDay'],
+        quantity: parseFloat(input.value)
+      });
+    }
+
+    console.log(purchases)
+
+    //TODO - send to API
+  }
+
+  save() {
+    this.back();
+    this.getInputValues();
   }
 
   back() {
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     API : "http://localhost:8100/api/",
-    version: '0.1.11'
+    version: '0.1.12'
 };
