c05654bd4c98dc873cd9efab3a6def66754d3927
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-03-24 21:53:26 -0300

    refactor: extended form base mixin in supplier form

--- src/app/pages/supplier/supplier-form/supplier-form.component.ts
+++ src/app/pages/supplier/supplier-form/supplier-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
-import { SupplierService } from '../services/supplier.service';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { FormBaseMixin } from 'src/app/shared/mixins/form-base.mixin';
 import { Supplier } from '../models/supplier';
+import { SupplierService } from '../services/supplier.service';
+
 
 @Component({
     selector: 'app-supplier-form',
     templateUrl: './supplier-form.component.html',
     styleUrls: ['./supplier-form.component.css']
 })
-export class SupplierFormComponent {
+export class SupplierFormComponent extends FormBaseMixin {
 
     supplierForm!: FormGroup;
     supplierId: string | any = '';
@@ -19,9 +22,10 @@ export class SupplierFormComponent {
         private formBuilder: FormBuilder,
         private router: Router,
         private route: ActivatedRoute,
-        private renderer: Renderer2,
-        private el: ElementRef
+        renderer: Renderer2,
+        el: ElementRef
     ) {
+        super(renderer, el);
         this.supplierForm = this.formBuilder.group({
             name: ['', Validators.required],
             address: this.formBuilder.group({
@@ -37,7 +41,7 @@ export class SupplierFormComponent {
             active: [true]
         });
 
-        this.observeAllControlChanges();
+        this.observeAllControlChanges(this.supplierForm);
 
     }
 
@@ -83,84 +87,12 @@ export class SupplierFormComponent {
     save() {
         if (this.supplierForm.valid) {
             const supplierForm = this.supplierForm.value;
-    
-            if (this.supplierId) {
-                this.supplierService.put(this.supplierId, supplierForm).subscribe({
-                    next: (response: any) => {
-                        console.log(response);
-                    },
-                    error: (error: any) => {
-                        console.log(error);
-                    },
-                    complete: () => {
-                        this.router.navigate(['/suppliers']);
-                    }
-                });
-            } else {
-                this.supplierService.post(supplierForm).subscribe({
-                    next: (response: any) => {
-                        console.log(response);
-                    },
-                    error: (error: any) => {
-                        console.log(error);
-                    },
-                    complete: () => {
-                        this.router.navigate(['/suppliers']);
-                    }
-                });
-            }
+            this.supplierService.save(supplierForm, this.supplierId).subscribe({
+                error: (error: any) => {console.error(error)},
+                complete: () => {this.router.navigate(['/suppliers'])}
+            });
         } else {
-            for (const controlName in this.supplierForm.controls) {
-                if (this.supplierForm.controls.hasOwnProperty(controlName)) {
-                    const control = this.supplierForm.get(controlName);
-            
-                    if (control instanceof FormGroup) {
-                        for (const nestedControlName in control.controls) {
-                            if (control.controls.hasOwnProperty(nestedControlName)) {
-                                const nestedControl = control.get(nestedControlName);
-                                this.handleControlValidation(nestedControl, nestedControlName);
-                            }
-                        }
-                    } else {
-                        this.handleControlValidation(control, controlName);
-                    }
-                }
-            }
-        }
-    }
-
-    private handleControlValidation(control: AbstractControl | null, controlName: string): void {
-        if (control?.hasError('required')) {
-            const inputElement = this.el.nativeElement.querySelector(`[formcontrolname="${controlName}"]`);
-            this.renderer.addClass(inputElement, 'is-invalid');
-        }
-    }
-    
-
-    private observeAllControlChanges() {
-        for (const controlName in this.supplierForm.controls) {
-            const control = this.supplierForm.get(controlName);
-            if (control) {
-                control.valueChanges.subscribe(() => {
-                    if (control instanceof FormGroup) {
-                        for (const nestedControlName in control.controls) {
-                            if (control.controls.hasOwnProperty(nestedControlName)) {
-                                const nestedControl = control.get(nestedControlName);
-                                this.handleControlChange(nestedControl, nestedControlName);
-                            }
-                        }
-                    } else {
-                        this.handleControlChange(control, controlName);
-                    }
-                });
-            }
-        }
-    }
-    
-    private handleControlChange(control: AbstractControl | null, controlName: string): void {
-        const inputElement = this.el.nativeElement.querySelector(`[formcontrolname="${controlName}"].form-control`);
-        if (inputElement) {
-            this.renderer.removeClass(inputElement, 'is-invalid');
+            this.setInvalidFields(this.supplierForm);
         }
     }
 
