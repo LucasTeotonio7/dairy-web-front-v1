f48c7eb42fb3722571557cd1744167ca87d1fa2d
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2024-04-29 20:12:27 -0300

    feat: created supplier payment service

---
+++ src/app/pages/purchase/services/supplier-payment.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { CrudService } from 'src/app/shared/services/crud.service';
+import { SupplierPayment } from '../models/supplier_payment';
+import { TokenService } from '../../auth/services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierPaymentService extends CrudService<SupplierPayment> {
+
+  constructor(http: HttpClient, tokenService: TokenService) {
+    super(http, tokenService, 'supplier-payment/');
+  }
+}
